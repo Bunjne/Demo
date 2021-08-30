21fb5973842625a89ca9e1072f7ba3fbe87a1e1b
Autthakrit <Autthakrit.C@genxas.com>
2021-08-30 13:41:01 +0700

    Init value extension

---
+++ library/src/main/java/whiz/sspark/library/extension/ValueExtension.kt
@@ -0,0 +1,11 @@
+package whiz.sspark.library.extension
+
+import android.content.Context
+import android.util.TypedValue
+import androidx.core.content.ContextCompat
+
+fun Float.toDP(context: Context) = (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics))
+
+fun Int.toDP(context: Context) = (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics)).toInt()
+
+fun Int.toResourceColor(context: Context) = ContextCompat.getColor(context, this)
