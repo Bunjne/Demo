89be68ff554765076b09286efd3271739b7f41ea
Autthakrit.C <autthakrit.c@ganxas.com>
2021-09-02 10:23:45 +0700

    revert sspark font

--- library/src/main/java/whiz/sspark/library/view/general/SSparkTextView.kt
+++ library/src/main/java/whiz/sspark/library/view/general/SSparkTextView.kt
@@ -3,54 +3,27 @@ package whiz.sspark.library.view.general
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.res.ResourcesCompat
 import whiz.sspark.library.R
 import whiz.sspark.library.SSparkLibrary
-import java.lang.Exception
 
 class SSparkTextView : AppCompatTextView {
-    constructor(context: Context) : super(context) { init(null) }
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) { init(attrs) }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { init(attrs) }
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) { style(attrs) }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { style(attrs) }
 
-    private fun init(attrs: AttributeSet?) {
+    private fun style(attrs: AttributeSet?) {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.SSparkTextView)
-        try {
-            val enum = attributes
-        } catch (e: Exception) {
-
-        }
         for (index in 0 until attributes.indexCount) {
             val attribute = attributes.getIndex(index)
             when (attribute) {
                 R.styleable.SSparkTextView_ssparkFont -> {
                     val fontTypeValue = attributes.getInt(index, 0)
                     val fontTypeface = when (fontTypeValue) {
-                        0 -> if (SSparkLibrary.projectType != null) {
-                            SSparkLibrary.boldTypeface
-                        } else {
-                            ResourcesCompat.getFont(context, R.font.noto_sans_thai_semi_bold)
-                        }
-                        1 -> if (SSparkLibrary.projectType != null) {
-                            SSparkLibrary.boldSerifTypeface
-                        } else {
-                            ResourcesCompat.getFont(context, R.font.noto_sans_thai_semi_bold)
-                        }
-                        2 -> if (SSparkLibrary.projectType != null) {
-                            SSparkLibrary.regularTypeface
-                        } else {
-                            ResourcesCompat.getFont(context, R.font.noto_sans_thai_regular)
-                        }
-                        3 -> if (SSparkLibrary.projectType != null) {
-                            SSparkLibrary.regularSerifTypeface
-                        } else {
-                            ResourcesCompat.getFont(context, R.font.noto_sans_thai_regular)
-                        }
-                        else ->  if (SSparkLibrary.projectType != null) {
-                            SSparkLibrary.regularTypeface
-                        } else {
-                            ResourcesCompat.getFont(context, R.font.noto_sans_thai_regular)
-                        }
+                        0 -> SSparkLibrary.boldTypeface
+                        1 -> SSparkLibrary.boldSerifTypeface
+                        2 -> SSparkLibrary.regularTypeface
+                        3 -> SSparkLibrary.regularSerifTypeface
+                        else -> SSparkLibrary.regularTypeface
                     }
 
                     typeface = fontTypeface
