a696ac439657899833fe0daba8539880ba36fdab
Autthakrit <Autthakrit.C@genxas.com>
2021-09-02 09:40:56 +0700

    Temp commit

---
+++ library/src/main/java/whiz/sspark/library/view/widget/menu/MenuCalendarWidget.kt
@@ -0,0 +1,28 @@
+package whiz.sspark.library.view.widget.menu
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import whiz.sspark.library.R
+import whiz.sspark.library.databinding.ViewMenuCalendarWidgetBinding
+import whiz.sspark.library.extension.show
+
+class MenuCalendarWidget: ConstraintLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private val binding by lazy {
+        ViewMenuCalendarWidgetBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    fun init() {
+        binding.ivGradient.show(R.drawable.bg_primary_gradient_0)
+        binding.ivArrow.show(R.drawable.ic_arrow_right)
+
+        binding.tvDay.text = ""
+        binding.tvDate.text = ""
+        binding.tvTerm.text = resources.getString(R.string.menu_semester_start, "")
+    }
+}
