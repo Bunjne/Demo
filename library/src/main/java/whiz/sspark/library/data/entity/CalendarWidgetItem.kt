ffc4bb23ffdc99cbe4b5e39a70c493d653986cf2
Autthakrit <Autthakrit.C@genxas.com>
2021-09-07 18:47:30 +0700

    Remove serialize name

--- library/src/main/java/whiz/sspark/library/data/entity/CalendarWidgetItem.kt
+++ library/src/main/java/whiz/sspark/library/data/entity/CalendarWidgetItem.kt
@@ -1,9 +1,8 @@
 package whiz.sspark.library.data.entity
 
-import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class CalendarWidgetItem(
-    @SerializedName("title") val title: String = "",
-    @SerializedName("date") val date: Date? = null
+    val title: String = "",
+    val date: Date? = null
 )
