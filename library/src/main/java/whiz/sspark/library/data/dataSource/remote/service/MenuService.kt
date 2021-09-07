5a5b8fc0b8775ec2abf25256ff3de14a15a27c64
Autthakrit <Autthakrit.C@genxas.com>
2021-09-07 18:40:39 +0700

    Implement menu widget API

--- library/src/main/java/whiz/sspark/library/data/dataSource/remote/service/MenuService.kt
+++ library/src/main/java/whiz/sspark/library/data/dataSource/remote/service/MenuService.kt
@@ -7,4 +7,16 @@ import whiz.sspark.library.data.entity.ApiResponseX
 interface MenuService {
     @GET("v1/menu")
     suspend fun getMenu(): Response<ApiResponseX>
+
+    @GET("v1/menu/grade_summary")
+    suspend fun getGradeSummary(): Response<ApiResponseX>
+
+    @GET("v1/menu/calendar")
+    suspend fun getCalendar(): Response<ApiResponseX>
+
+    @GET("v1/menu/notification_inbox")
+    suspend fun getNotificationInbox(): Response<ApiResponseX>
+
+    @GET("v1/menu/advising_note")
+    suspend fun getAdvisingNote(): Response<ApiResponseX>
 }
