9c167e46d6f52e4414f64e0233e870d342033242
Autthakrit <Autthakrit.C@genxas.com>
2021-09-08 10:27:27 +0700

    Refactor code

--- library/src/main/java/whiz/sspark/library/data/entity/MenuGradeSummaryDTO.kt
+++ library/src/main/java/whiz/sspark/library/data/entity/MenuGradeSummaryDTO.kt
@@ -6,7 +6,7 @@ import whiz.sspark.library.utility.localize
 data class MenuGradeSummaryDTO (
     @SerializedName("nameEn") val nameEn: String = "",
     @SerializedName("nameTh") val nameTh: String = "",
-    @SerializedName("value") val value: Float = 0f,
+    @SerializedName("value") val value: Float? = null,
     @SerializedName("fullValue") val fullValue: Float = 0f,
     @SerializedName("colorCode1") val colorCode1: String = "",
     @SerializedName("colorCode2") val colorCode2: String = "",
@@ -22,10 +22,10 @@ data class MenuGradeSummaryCourseDTO (
     @SerializedName("nameTh") val nameTh: String = "",
     @SerializedName("nameCn") val nameCn: String = "",
     @SerializedName("credits") val credits: Int = 0,
-    @SerializedName("value") val value: Float = 0f,
+    @SerializedName("value") val value: Float? = null,
     @SerializedName("fullValue") val fullValue: Float = 0f
 )
 
 fun convertToGradeSummaryItem(grades: List<MenuGradeSummaryDTO>): List<GradeSummary> {
-    return grades.map { GradeSummary(name = it.name, startColorCode = it.colorCode1, endColorCode = it.colorCode2, grade = it.value) }
+    return grades.filter { it.value != null }.map { GradeSummary(name = it.name, startColorCode = it.colorCode1, endColorCode = it.colorCode2, grade = it.value!!) }
 }
--- library/src/main/java/whiz/sspark/library/data/entity/MenuItem.kt
+++ library/src/main/java/whiz/sspark/library/data/entity/MenuItem.kt
@@ -1,9 +1,7 @@
 package whiz.sspark.library.data.entity
 
-import com.google.gson.annotations.SerializedName
-
 data class MenuItem(
-    @SerializedName("code") val code: String = "",
-    @SerializedName("name") val name: String = "",
-    @SerializedName("imageUrl") val imageUrl: String = ""
-    )
+    val code: String = "",
+    val name: String = "",
+    val imageUrl: String = ""
+)
