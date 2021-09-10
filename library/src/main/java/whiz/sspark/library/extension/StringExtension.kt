1907b7b9ee9ec2c088c0d9e9e8934d19ebc5a97e
P <chayapam.s@genxas.com>
2021-09-10 10:24:47 +0700

    Added extension for create QRCodeDrawable with Zxing.

---
+++ library/src/main/java/whiz/sspark/library/extension/StringExtension.kt
@@ -0,0 +1,38 @@
+package whiz.sspark.library.extension
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+
+fun String.toQRCodeDrawable(context: Context, size: Int): Drawable? {
+    val result: BitMatrix? = try {
+        MultiFormatWriter().encode(
+            this,
+            BarcodeFormat.QR_CODE,
+            size,
+            size,
+            mapOf(EncodeHintType.MARGIN to 0)
+        )
+    } catch (writerException: WriterException) {
+        null
+    }
+    return result?.let {
+        val width = result.width
+        val height = result.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val colorPixel = if (result.get(x, y)) Color.BLACK else Color.WHITE
+                bitmap.setPixel(x, y, colorPixel)
+            }
+        }
+        BitmapDrawable(context.resources, bitmap)
+    }
+}
