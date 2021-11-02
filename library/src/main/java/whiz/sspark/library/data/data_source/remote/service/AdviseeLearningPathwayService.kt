8671989d87886e02c60e0b3b3119dc1c41fee78b
Autthakrit <Autthakrit.C@genxas.com>
2021-11-02 09:33:58 +0700

    Change path

--- library/src/main/java/whiz/sspark/library/data/data_source/remote/service/AdviseeLearningPathwayService.kt
+++ library/src/main/java/whiz/sspark/library/data/data_source/remote/service/AdviseeLearningPathwayService.kt
@@ -3,9 +3,8 @@ package whiz.sspark.library.data.data_source.remote.service
 import retrofit2.Response
 import retrofit2.http.*
 import whiz.sspark.library.data.entity.ApiResponseX
-import whiz.sspark.library.data.entity.LearningPathwayCourseAPIBody
 
 interface AdviseeLearningPathwayService: LearningPathwayService {
-    @GET("api/v1/pathways/me") //TODO wait confirm path
-    suspend fun getLearningPathway(@Query("studentId") studentId: String): Response<ApiResponseX>
+    @GET("api/v1/pathways/{studentId}")
+    suspend fun getLearningPathway(@Path("studentId") studentId: String): Response<ApiResponseX>
 }
